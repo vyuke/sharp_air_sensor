fe1915c590e721a569f91d6151b1e074610b5ed6
yuke <1105313600@qq.com>
2017-01-11 11:51:22 +0800

    Update sharp_air_sensor.ino

--- sharp_air_sensor.ino
+++ sharp_air_sensor.ino
@@ -4,20 +4,20 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2); 
 int measurePin = 0; //Connect dust sensor to Arduino A0 pin
 int ledPower = 2;   //Connect 3 led driver pins of dust sensor to Arduino D2
-
+ 
 int samplingTime = 280;
 int deltaTime = 40;
 int sleepTime = 9680;
-
+ 
 float voMeasured = 0;
 float calcVoltage = 0;
 float dustDensity = 0;
-
+ 
 void setup(){
-
+ 
   Serial.begin(9600);
   pinMode(ledPower,OUTPUT);
-
+ 
   lcd.init();                     
   lcd.backlight();
   lcd.setCursor(5, 0);
@@ -28,8 +28,13 @@ void setup(){
     lcd.write(0xff);
     delay(250);
   }
+  lcd.init();
+  lcd.setCursor(7,1);
+  lcd.print("-mg/m^3");                     
+  lcd.setCursor(0, 0);
+  lcd.print("air dust density");
 }
-
+ 
 void loop(){
   digitalWrite(ledPower,LOW); 
   delayMicroseconds(samplingTime);
@@ -39,24 +44,22 @@ void loop(){
   delayMicroseconds(sleepTime);
   calcVoltage = voMeasured * (5.0 / 1024.0);
   dustDensity = 0.17 * calcVoltage - 0.1;
-
+  if(dustDensity<0)dustDensity=0;
   Serial.print("Raw Signal Value (0-1023): ");
   Serial.print(voMeasured);
-
+ 
   Serial.print(" - Voltage: ");
   Serial.print(calcVoltage);
-
+ 
   Serial.print(" - Dust Density: ");
   Serial.println(dustDensity); // unit: mg/m3
-
-  lcd.init();                     
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("dustDensity:");
-  lcd.setCursor(0,1);
+ 
+//  lcd.init();                     
+//  lcd.backlight();
+//  lcd.setCursor(0, 0);
+//  lcd.print("dustDensity:");
+  lcd.setCursor(3,1);
   lcd.print(dustDensity);
-  lcd.setCursor(9,1);
-  lcd.print(" mg/m^3");
-  delay(1000);
+ 
+  delay(5000);
 }
-
